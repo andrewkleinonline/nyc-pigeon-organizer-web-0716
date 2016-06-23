b7e0e02b7a9a9bf8f7dd08cb951c77278e03e801
quarthex15 <andrew.klein.j@gmail.com>
2016-06-22 21:50:42 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+
+  data[:color].each do |color, pigeons|
+    pigeons.each do |pigeon|
+      if !(pigeon_list.keys.include?(pigeon))
+        pigeon_list[pigeon] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+
+  data.each do |attribute, sub_hash|
+    sub_hash.each do |value, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon][attribute] << value.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end
